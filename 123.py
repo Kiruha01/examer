82ba574d67af3c0389cde18cc645b01c0d563af5
Kiruha01 <liss-2001g@mail.ru>
2018-06-09 14:06:30 +0500

    v1

--- 123.py
+++ 123.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, json
+import main
 
 
 app = Flask(__name__)
@@ -13,4 +14,10 @@ def handler():
     data = json.loads(request.data)
     if data['type'] == 'confirmation':
         return 'bf65672b'
+    elif data["type"] == "message_new":
+        user_id = data['object']['user_id']
+        text = data['object']['body']
+        main.main(user_id, text)
+        return 'ok'
+
 
---
+++ main.py
@@ -0,0 +1,39 @@
+token = '6be1dc7d81306eaa2394e1b512fa2ce7b6ef4432aef65ac1523513ec6495e2b753fbf06afc305d565ce6b'
+
+import vk_api
+from examer import Examer
+
+vk = vk_api.vk_api.VkApi(token=token)
+
+
+memory = {}
+
+def main(id, text):
+    global memory
+    if text == 'Привет':
+        vk.method('messages.send', {'user_id': id, 'message': 'Кидай ссылку на тест и я решу его за тебя'})
+    elif text == 'reset' and str(id) == '276820555':
+        memory = {}
+        vk.method('messages.send', {'user_id': '276820555', 'message': 'ok'})
+    else:
+        link = text.split('/')[-1]
+        if link in memory:
+            for msg in memory[link]:
+                vk.method('messages.send', {'user_id': id, 'message': msg})
+        else:
+            ex = Examer('arkadiy@p33.org', 'zabylkto01')
+            ex.set_link(text)
+            try:
+                ex.start()
+            except ArithmeticError:
+                vk.method('messages.send', {'user_id': id, 'message': 'Invalid Link'})
+            else:
+                ex.format_text()
+                list_ = []
+                for task_id in ex.list_of_task:
+                    list_.append(task_id['question'] + '\nОтвет: ' + task_id['answer'])
+                memory[link] = list_
+                main(id, text)
+    
+
+
