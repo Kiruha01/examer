297aadc28578afa5e2c78f82ebfbd9bbbf509d07
Kiruha01 <liss-2001g@mail.ru>
2021-12-23 16:02:57 +0300

    Add async methods to get answers

--- examer.py
+++ examer.py
@@ -1,10 +1,16 @@
+import asyncio
 import hashlib
 from typing import List, Dict
 
+import aiohttp
 from bs4 import BeautifulSoup
 import requests
+from concurrent import futures
+
+from asyncio import FIRST_EXCEPTION
 
 from ExamerException import *
+import time
 
 
 class Task:
@@ -18,12 +24,13 @@ class Task:
     :field answer: str - правильный ответ
 
     """
+
     def __init__(self, task_dict: Dict[str, str]):
         self.id: str = task_dict['id']
         self.question: str = self.__remove_tags(task_dict['task_text'])
         self.difficult: int = self.__convert_dif(task_dict['difficult'])
         self.avg_time: float = float(task_dict['avg_time'])
-        self.answer = None
+        self.answer = "No answer"
 
     @staticmethod
     def __remove_tags(text: str) -> str:
@@ -49,6 +56,7 @@ class ExamerTest:
         :field tasks: Dist[str, Task] - словарь заданий вида ("task_id": Task)
         :field avg_time: int - примерное время выполнения теста
     """
+
     def __init__(self, test_dict: Dict):
         self.theme: str = test_dict['title']
         self.id_test: str = str(test_dict['scenarioId'])
@@ -84,6 +92,7 @@ class Examer(object):
         :raises SignError: ошибка отправки запроса решистрации
         :raises TeacherError: пользователь не является учителем
         """
+        self.MAX_REQUESTS = 10
         self.BASE_URL = "https://examer.ru/"
         self.SIGN_POSTFIX = 'Ic8_31'
         self.session = requests.session()
@@ -137,6 +146,39 @@ class Examer(object):
                     test.unprocessed_tasks_id.remove(task['id'])
         return test
 
+    def async_get_test(self, link: str):
+        async def gen_test(test: ExamerTest, session):
+            payload = {'sid': '3', 'scenario': '1', 'id': test.id_test, 'title': test.theme, 'easy': '12', 'normal': '12',
+                       'hard': '12'}
+
+            async with session.post('https://teacher.examer.ru/api/v2/teacher/test', data=payload) as resp:
+                data = await resp.json()
+                for task in data['tasks']:
+                    if task['id'] in test.unprocessed_tasks_id:
+                        test.tasks[task['id']].answer = task['answer']
+                        test.unprocessed_tasks_id.remove(task['id'])
+                if len(test.unprocessed_tasks_id) == 0:
+                    raise StopIteration
+
+        async def asynchronous(test: ExamerTest):
+            async with aiohttp.ClientSession(cookies=self.session.cookies.get_dict()) as session:
+                tasks = [asyncio.create_task(gen_test(test, session))
+                         for i in range(self.MAX_REQUESTS)]
+
+                _, pendings = await asyncio.wait(tasks, return_when=FIRST_EXCEPTION)
+                for p in pendings:
+                    p.cancel()
+
+
+        test_id = link.split('/')[-1]
+        test = self.get_questions(test_id)
+
+        i = 0
+        while len(test.unprocessed_tasks_id):
+            print('new')
+            asyncio.run(asynchronous(test))
+        return test
+
     def prepare_auth_request_params(self, email: str, password: str, token: str) -> Dict[str, str]:
         """
         Подготовка параметров для регистрации и добавление подписи.
@@ -173,7 +215,18 @@ class Examer(object):
         :param test_theme: str - название темы теста
         :return: Dict
         """
-        payload = {'sid': '3', 'scenario': '1', 'id': test_id, 'title': test_theme, 'easy': '6', 'normal': '7',
-                   'hard': '7'}
+        payload = {'sid': '3', 'scenario': '1', 'id': test_id, 'title': test_theme, 'easy': '12', 'normal': '12',
+                   'hard': '12'}
         return self.session.post(url='https://teacher.examer.ru/api/v2/teacher/test',
                                  data=payload).json()
+
+
+if __name__ == "__main__":
+    ex = Examer('hawetoh999@hagendes.com', '123456789')
+    print("starting")
+    time_s = time.monotonic()
+    print(ex.get_test('https://t.examer.ru/f9afa'))
+    print("Sync ", time.monotonic() - time_s)
+    time_s = time.monotonic()
+    print(ex.async_get_test('https://t.examer.ru/f9afa'))
+    print("Async ", time.monotonic() - time_s)
